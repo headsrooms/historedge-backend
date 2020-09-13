45a5ef643da5da806e2ad52d6f0902af3cc265cf
Pablo Cabezas <headsrooms@gmail.com>
2020-09-13 16:46:48 +0200

    Add is_processed query_param to filter given results

--- historedge_backend/api/endpoints/page_visits.py
+++ historedge_backend/api/endpoints/page_visits.py
@@ -7,12 +7,18 @@ from historedge_backend.schemas import OutputPageVisitListSchema
 
 
 async def get_page_visits(request: Request) -> UJSONResponse:
+    is_processed = request.query_params.get("is_processed")
     page = int(request.query_params.get("page", 1))
     page_size = int(request.query_params.get("page_size", 10))
     offset = (page - 1) * page_size if page != 1 else 0
 
-    pages = PageVisit.all().limit(page_size).offset(offset)
-    count = await PageVisit.all().count()
+    if is_processed:
+        is_processed = is_processed == "true" or is_processed == "True"
+        pages = PageVisit.filter(is_processed=is_processed)
+    else:
+        pages = PageVisit.all()
+    count = await pages.count()
+    pages = pages.limit(page_size).offset(offset)
     pages = await OutputPageVisitListSchema.from_queryset(pages)
     response = orjson.loads(pages.json())
     return UJSONResponse({"count": count, "page_visits": response})
