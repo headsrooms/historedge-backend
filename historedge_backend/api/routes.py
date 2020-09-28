c9abaf4d282a76f091cb9045b6930737f788a246
Pablo Cabezas <headsrooms@gmail.com>
2020-09-28 14:57:38 +0200

    Add new API route

--- historedge_backend/api/routes.py
+++ historedge_backend/api/routes.py
@@ -2,7 +2,7 @@ from starlette.routing import Route
 
 from historedge_backend.api.endpoints.bookmarks import bookmark_page, get_bookmarks
 from historedge_backend.api.endpoints.history import register_history
-from historedge_backend.api.endpoints.page_visits import get_page_visits
+from historedge_backend.api.endpoints.page_visits import get_page_visits, distribute_page_visits_to_scraper
 from historedge_backend.api.endpoints.pages import visit_page, get_pages
 from historedge_backend.api.endpoints.users import create_user
 
@@ -14,6 +14,7 @@ routes = [
     Route("/pages/", get_pages, methods=["GET"]),
     # page_visits
     Route("/page-visits/", get_page_visits, methods=["GET"]),
+    Route("/page-visits/", distribute_page_visits_to_scraper, methods=["POST"]),
     # history
     Route("/history/", register_history, methods=["POST"]),
     # bookmarks
