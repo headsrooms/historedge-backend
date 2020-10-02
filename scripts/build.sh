8b530f91618cf92691ee030eb03ede687f225029
Pablo Cabezas <headsrooms@gmail.com>
2020-10-02 13:55:50 +0200

    Add specific dockerfile for scraper

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
-docker build . -t headsrooms/historedge
-docker push headsrooms/historedge
+docker build . -t headsrooms/historedge && docker push headsrooms/historedge
+docker build -f scraper.Dockerfile . -t headsrooms/historedge-scraper && docker push headsrooms/historedge-scraper
